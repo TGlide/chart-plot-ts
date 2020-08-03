3db20aa21a59bb6341481dd213ddc47280b1eca4
tglide <thomglopes@gmail.com>
2020-08-03 13:35:53 -0300

    add sass

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./styles/app.scss";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="hero is-primary">
+        <div className="hero-body">
+          <div className="container">
+            <div className="header">
+              <img
+                src={require("./assets/logo.png")}
+                alt="Intelie - a RigNet company"
+              />
+              <div className="title"> Thomas Gouveia Lopes Challenge </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <section className="section chart-input">
+        <div className="container">
+          <textarea name="" id=""></textarea>
+        </div>
+      </section>
+
+      <div className="footer">
+        <div className="container">
+          <button className="button is-info">Generate Chart</button>
+        </div>
+      </div>
     </div>
   );
 }
